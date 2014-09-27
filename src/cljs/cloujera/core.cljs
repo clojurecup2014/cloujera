47191d03965bea9bb4fac4705da3b4c4ca5e0669
Keigo Suzukawa <k.suzukawa@gmail.com>
2014-09-27 22:53:16 +0100

    Update rest-client to use transit

--- src/cljs/cloujera/core.cljs
+++ src/cljs/cloujera/core.cljs
@@ -1,29 +1,27 @@
 (ns cloujera.core
   (:require [om.core :as om :include-macros true]
             [om.dom :as dom :include-macros true]
-            [cloujera.rest-client :as rest-client]))
+            [cloujera.rest-client :as rest]))
 
 (enable-console-print!)
-
 
 (def app-state
   (atom
     {:videos [{}]}))
 
+(defn process-response [response]
+  (println (str "Search results are: " response)))
+
+
+;; handlers
+
 (defn handle-input [e owner {:keys [text]}]
   (om/set-state! owner :text (.. e -target -value)))
 
-(defn process-response [response]
-  (println (str "Search results: " response)))
-
-
-(defn search [app owner]
-  (let [input (om/get-node owner "search-query")
-        search-query (-> input .-value)]
-    (when search-query
-      (println (str "Search query=" search-query))
-      (rest-client/search search-query process-response))))
-
+(defn handle-search [app owner]
+  (let [search-query (.-value (om/get-node owner "search-query"))]
+    (println (str "Search query is: " search-query))
+    (rest/search search-query process-response)))
 
 ;; search bar component
 (defn search-bar-view [app owner]
@@ -39,7 +37,7 @@
                       :ref "search-query"
                       :value (:text state)
                       :onChange #(handle-input % owner state)}
-      (dom/button #js {:onClick #(search app owner)}
+      (dom/button #js {:onClick #(handle-search app owner)}
                       "Search")))))
 
 (om/root
--- src/cljs/cloujera/rest-client.cljs
+++ src/cljs/cloujera/rest-client.cljs
@@ -1,16 +1,16 @@
 (ns cloujera.rest-client
-  (:require [ajax.core :refer [GET POST]]))
+  (:require [cognitect.transit :as transit]
+            [goog.events :as events])
+  (:import [goog.net XhrIo]))
 
 
-(def ^:private meths
-  {:get "GET"})
+(def ^:private uri "http://127.0.0.1:8080")
+(def ^:private json-reader (transit/reader :json))
 
-(def ^:private uri
-  "http://127.0.0.1:8080")
-
-(defn search [query handler]
-  (let [search-endpoint (str uri "/search")]
-    (GET search-endpoint
-        {:params {:query query}
-         :handler handler })))
-
+(defn search [search-query call-back]
+  (let [xhr (XhrIo.)]
+    (events/listen xhr goog.net.EventType.COMPLETE
+      (fn [e]
+        (call-back (transit/read json-reader (.getResponseText xhr)))))
+    (. xhr
+      (send (str uri "/search?query=" search-query) "GET"))))
