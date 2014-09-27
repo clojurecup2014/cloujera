3e7f08a063d02b8d3eea1ced307d090327dd5f43
portovep <portovep@tcd.ie>
2014-09-28 00:32:47 +0100

    Add result pane Om component to render results Update response callback to update app state with new results

--- src/cljs/cloujera/core.cljs
+++ src/cljs/cloujera/core.cljs
@@ -1,27 +1,26 @@
 (ns cloujera.core
   (:require [om.core :as om :include-macros true]
             [om.dom :as dom :include-macros true]
-            [cloujera.rest-client :as rest]))
+            [cloujera.rest-client :as rest-client]))
 
 (enable-console-print!)
 
 (def app-state
   (atom
-    {:videos [{}]}))
-
-(defn process-response [response]
-  (println (str "Search results are: " response)))
+    {:videos []}))
 
 
 ;; handlers
-
 (defn handle-input [e owner {:keys [text]}]
   (om/set-state! owner :text (.. e -target -value)))
 
 (defn handle-search [app owner]
   (let [search-query (.-value (om/get-node owner "search-query"))]
     (println (str "Search query is: " search-query))
-    (rest/search search-query process-response)))
+    (rest-client/search search-query
+                        (fn [response]
+                           (println "Server response:" response)
+                           (swap! app-state assoc :videos (get response "results"))))))
 
 ;; search bar component
 (defn search-bar-view [app owner]
@@ -44,3 +43,30 @@
   search-bar-view
   app-state
   {:target (. js/document (getElementById "search-bar"))})
+
+
+;; result pane component
+(defn display-video [video]
+  (str "title: " (get video "title") ", id: " (get video "id")))
+
+(defn video-view [video owner]
+  (reify
+    om/IRender
+    (render [this]
+            (dom/li nil
+              (dom/span nil (if-not (nil? video)
+                              (display-video video)))))))
+
+(defn result-pane-view [app owner]
+  (reify
+
+    om/IRender
+    (render [this]
+      (dom/h2 nil "Results")
+      (apply dom/ul nil
+         (om/build-all video-view (:videos app))))))
+
+(om/root
+  result-pane-view
+  app-state
+  {:target (. js/document (getElementById "result-pane"))})
