91bbbb25109c08567a14d1c267d5b3c25921182b
Martino Visintin <mvisinti@thoughtworks.com>
2014-09-27 14:50:09 +0200

    Moved scraping into burglar and namspaced the lot under burglar

--- src/clj/cloujera/burglar/core.clj
+++ src/clj/cloujera/burglar/core.clj
@@ -1,35 +1,23 @@
 (ns cloujera.burglar.core
-  (:require [clj-http.client :as http]
-            [clj-http.cookies :as cookies]))
+  (:require [cloujera.burglar.scraper :as scraper]
+            [cloujera.cache.core :as cache]))
 
-;; inspiration from: https://github.com/coursera-dl/coursera/blob/master/coursera%2Fcookies.py
-(defn- authenticated-cookie-store
-  "returns a cookie store that lets you get protected pages"
-  [username password]
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(def username "vise890+cloujera@gmail.com")
+(def password "letswinthisthing")
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-  (let [cs (cookies/cookie-store)
-        a-mooc-url "https://class.coursera.org/modernpoetry-003/lecture"]
+(defn- coursera-urls []
+  ["https://class.coursera.org/modernpoetry-003/lecture"
+   "https://class.coursera.org/comparch-003/lecture"
+   "https://class.coursera.org/algs4partI-006/lecture"
+   "https://class.coursera.org/calcsing-006/lecture"
+   "https://class.coursera.org/automata-003/lecture"
+   "https://class.coursera.org/crypto-012/lecture"
+   "https://class.coursera.org/ml-007/lecture"])
 
-    ;; hit a MOOC to get the CSRF token
-    (http/get a-mooc-url {:cookie-store cs})
+(def get-lectures-page (cache/red-cache (scraper/authenticated-get username password)))
 
-    ;; login
-    (let [login-url "https://accounts.coursera.org/api/v1/login"
-          csrf-token (:crf_token (cookies/get-cookies cs))
-          headers {"Cookie" (str "csrftoken=" csrf-token)
-                   "Referer" "https://accounts.coursera.org/signin"
-                   "X-CSRFToken" csrf-token}
-          data {"email" username
-                "password" password
-                "webrequest" true}]
-
-      (http/post login-url {:headers headers
-                            :form-params  data
-                            :cookie-store cs})
-      cs)))
-
-;; Username -> Password -> (ProtectedUrl -> Page)
-(defn authenticated-get [username password]
-  (let [authenticated-cs (authenticated-cookie-store username password)]
-    (fn [protected-url]
-      (http/get protected-url {:cookie-store authenticated-cs}))))
+(->> (coursera-urls)
+     (map get-lectures-page)
+     (map :body))
---
+++ src/clj/cloujera/burglar/scraper.clj
@@ -0,0 +1,35 @@
+(ns cloujera.burglar.scraper
+  (:require [clj-http.client :as http]
+            [clj-http.cookies :as cookies]))
+
+;; inspiration from: https://github.com/coursera-dl/coursera/blob/master/coursera%2Fcookies.py
+(defn- authenticated-cookie-store
+  "returns a cookie store that lets you get protected pages"
+  [username password]
+
+  (let [cs (cookies/cookie-store)
+        a-mooc-url "https://class.coursera.org/modernpoetry-003/lecture"]
+
+    ;; hit a MOOC to get the CSRF token
+    (http/get a-mooc-url {:cookie-store cs})
+
+    ;; login
+    (let [login-url "https://accounts.coursera.org/api/v1/login"
+          csrf-token (:crf_token (cookies/get-cookies cs))
+          headers {"Cookie" (str "csrftoken=" csrf-token)
+                   "Referer" "https://accounts.coursera.org/signin"
+                   "X-CSRFToken" csrf-token}
+          data {"email" username
+                "password" password
+                "webrequest" true}]
+
+      (http/post login-url {:headers headers
+                            :form-params  data
+                            :cookie-store cs})
+      cs)))
+
+;; Username -> Password -> (ProtectedUrl -> Page)
+(defn authenticated-get [username password]
+  (let [authenticated-cs (authenticated-cookie-store username password)]
+    (fn [protected-url]
+      (http/get protected-url {:cookie-store authenticated-cs}))))
