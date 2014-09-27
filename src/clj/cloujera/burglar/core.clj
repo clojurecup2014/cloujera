fb49dbae7199b911b993d130c06e86a2099a10fd
Martino Visintin <mvisinti@thoughtworks.com>
2014-09-27 11:14:08 +0200

    move files in the new structure

---
+++ src/clj/cloujera/burglar/core.clj
@@ -0,0 +1,10 @@
+(ns cloujera.burglar.core
+  (:require [org.httpkit.client :as http]))
+
+
+
+(defn get-lecture-html [url]
+ "HELLO"
+
+  )
+
---
+++ src/clj/cloujera/core.clj
@@ -0,0 +1,15 @@
+(ns cloujera.core
+  (:require [cloujera.burglar.core :as burglar]))
+
+(defn- coursera-urls []
+  ["https://class.coursera.org/modernpoetry-003/lecture"
+   "https://class.coursera.org/comparch-003/lecture"
+   "https://class.coursera.org/algs4partI-006/lecture"
+   "https://class.coursera.org/calcsing-006/lecture"
+   "https://class.coursera.org/automata-003/lecture"
+   "https://class.coursera.org/crypto-012/lecture"
+   "https://class.coursera.org/ml-007/lecture"
+   ])
+
+(->> (coursera-urls)
+     (map burglar/get-lecture-html))
