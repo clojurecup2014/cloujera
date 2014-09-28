626ff2731693bb68cccfeaf702aa8af9a794fcf7
Michael Kay <kay.mikey@gmail.com>
2014-09-28 20:01:09 +0200

    Fix the stuff

--- src/clj/cloujera/controllers/scrounger.clj
+++ src/clj/cloujera/controllers/scrounger.clj
@@ -1,16 +1,11 @@
 ;; controller for dealing with search requests
 
 (ns cloujera.controllers.scrounger
-  (:require [cloujera.models.video :as video]))
+  (:require [cloujera.models.video :as video]
+            [cloujera.burglar.core :as burglar]
+            [cloujera.search.core :as search]
+            [schema.core :as s]))
 
 (defn search [query]
   (let [videos (search/term-matching query)]
-    (take 10(map video/valid-video? videos
-         ))))
-
-
-
-;; stub for API endpoint
-#_(defn search [query]
-(let [video (video/valid-video? video/fake-video)]
-    [video video video]))
+    (map video/valid-video? videos)))
