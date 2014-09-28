60bff05c26aafc1d5bcf41ed60ff5dc84532dbb6
Martino Visintin <mvisinti@thoughtworks.com>
2014-09-28 21:17:15 +0200

    move LEIN_ROOT export to avoid warnings

--- deploy.sh
+++ deploy.sh
@@ -12,8 +12,9 @@ sed -i 's/;(def ^:private uri "http:\/\/cloujera.clojurecup.com:80")/(def ^:priv
 # comment in
 sed -i 's/(def ^:private uri "http:\/\/127.0.0.1:8080")/;(def ^:private uri "http:\/\/127.0.0.1:8080")/' ./src/cljs/cloujera/rest-client.cljs
 
+export LEIN_ROOT="Something"
+
 /home/cloujera/bin/lein cljsbuild once
 
-export LEIN_ROOT=1
 
 sudo /home/cloujera/bin/lein run &
