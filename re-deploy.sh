78b53247eee2c2df2ee47763afe7af7c8891555c
Martino Visintin <mvisinti@thoughtworks.com>
2014-09-28 21:53:30 +0200

    improve comments

--- re-deploy.sh
+++ re-deploy.sh
@@ -7,10 +7,10 @@ sudo -v
 git checkout .
 git pull
 
-# comment out
+# remove from commented state the prod setting
 sed -i 's/;(def ^:private uri "http:\/\/cloujera.clojurecup.com:80")/(def ^:private uri "http:\/\/cloujera.clojurecup.com:80")/' ./src/cljs/cloujera/rest-client.cljs
 
-# comment in
+# comment out the dev setting
 sed -i 's/(def ^:private uri "http:\/\/127.0.0.1:8080")/;(def ^:private uri "http:\/\/127.0.0.1:8080")/' ./src/cljs/cloujera/rest-client.cljs
 
 
