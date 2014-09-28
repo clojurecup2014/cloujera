d42145a0eaa00d6335882e5e7074b19ce496f29c
Martino Visintin <mvisinti@thoughtworks.com>
2014-09-28 21:59:24 +0200

    update deploy script

--- re-deploy.sh
+++ re-deploy.sh
@@ -13,9 +13,14 @@ sed -i 's/;(def ^:private uri "http:\/\/cloujera.clojurecup.com:80")/(def ^:priv
 # comment out the dev setting
 sed -i 's/(def ^:private uri "http:\/\/127.0.0.1:8080")/;(def ^:private uri "http:\/\/127.0.0.1:8080")/' ./src/cljs/cloujera/rest-client.cljs
 
-
 /home/cloujera/bin/lein cljsbuild once
 
-
 export LEIN_ROOT="is set"
-sudo /home/cloujera/bin/lein run > /var/log/cloujera.log 2>&1 &
+
+export LOGFILE='/var/log/cloujera.log'
+
+# empty the logfile
+cat /dev/null > LOGFILE
+
+# start it up
+sudo `/home/cloujera/bin/lein run > LOGFILE 2>&1 &`
