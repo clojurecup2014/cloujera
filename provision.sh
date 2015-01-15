58bcb7a17fb54dad954c113b8e33d66d38a52fd7
Martino Visintin <mvisinti@thoughtworks.com>
2015-01-15 00:29:44 -0500

    always restart the redis and elasticsearch containers

--- provision.sh
+++ provision.sh
@@ -11,8 +11,8 @@ curl -sSL https://get.docker.com/ubuntu/ | sudo sh
 
 echo "==> setting up ElasticSearch"
 sudo docker pull dockerfile/elasticsearch
-sudo docker run --name elasticsearch -d -p 9200:9200 -p 9300:9300 dockerfile/elasticsearch
+sudo docker run --name elasticsearch --restart=always -d -p 9200:9200 -p 9300:9300 dockerfile/elasticsearch
 
 echo "==> setting up REDIS"
 sudo docker pull dockerfile/redis
-sudo docker run --name redis -d -p 6379:6379 dockerfile/redis
+sudo docker run --name redis --restart=always -d -p 6379:6379 dockerfile/redis
