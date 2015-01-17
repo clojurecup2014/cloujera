9fab19fdd9b961cb916179ccbe80411d0184d8b1
Martino Visintin <mvisinti@thoughtworks.com>
2015-01-16 20:09:49 -0500

    remove HTTP calls at compile time
    + add a FIXME for the memoization of the cookie store (not nice..)

--- src/clj/cloujera/burglar/core.clj
+++ src/clj/cloujera/burglar/core.clj
@@ -11,20 +11,22 @@
 (def password "letswinthisthing")
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(def get-coursera-page (cache/persist (scraper/get-protected-page username password)))
+(defn- get-coursera-page []
+  (cache/persist (memoize (scraper/get-protected-page username
+                                                      password))))
 
 ;; Video -> Video
 (defn- get-and-add-video-url [video]
   {:pre [(utils/not-nil? video)]}
   (let [embedded-video-url (video-parser/_link->embedded-video-url (:_link video))
-        embedded-video-page (get-coursera-page embedded-video-url)
+        embedded-video-page ((get-coursera-page) embedded-video-url)
         url (video-parser/extract-video-url embedded-video-page)]
     (assoc video :video-url url)))
 
 ;; THE BEAST
 (defn raid [lecture-url]
   {:pre [(utils/not-nil? lecture-url)]}
-  (let [lecture-page-html (get-coursera-page lecture-url)
+  (let [lecture-page-html ((get-coursera-page) lecture-url)
         videos-without-urls (parser/extract-videos lecture-page-html)
         all-videos (pmap get-and-add-video-url videos-without-urls)
         useable-videos (remove video-parser/no-embeddable-video? all-videos)]
