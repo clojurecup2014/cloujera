692392db4c90e39ca0af2126dd6bdcdef315bfa0
Martino Visintin <mvisinti@thoughtworks.com>
2015-01-18 15:54:15 -0500

    Setup Vagrant
    - local develpment is easier to setup (vagrant up)
    - local env is as close to prod as possible
    - `provision.sh` can be used on prod

---
+++ scripts/deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+sudo -v
+
+echo "==> pulling most recent version (git)"
+# FIXME: it would be nice to checkout to avoid weird
+# stuff happening, but it's really easy to run this
+# script fortest in the vagrant VM in /vagrant
+# and lose work this way...
+# git checkout .
+git pull
+
+echo "==> Building CLJS"
+lein cljsbuild once
+
+echo "==> Uberjarring"
+lein uberjar
+
+echo "==> Running cloujera"
+java -jar ./target/uberjar/cloujera-0.1.0-SNAPSHOT-standalone.jar > cloujera.log 2>&1
