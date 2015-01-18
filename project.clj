70e07d10229a4e61ec24fc9324cad0b055985353
Martino Visintin <mvisinti@thoughtworks.com>
2015-01-18 18:44:01 -0500

    bump all deps

--- project.clj
+++ project.clj
@@ -3,28 +3,24 @@
   :url "cloujera.clojurecup.com"
 
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2311"]
-                 [om "0.7.1"]
-                 [com.cognitect/transit-cljs "0.8.188"]
+                 [org.clojure/clojurescript "0.0-2665"]
+                 [om "0.7.3"]
+                 [com.cognitect/transit-cljs "0.8.199"]
 
-                 [prismatic/schema "0.3.0"]
-                 [compojure "1.1.9"] ;; for routing
-                 [ring "1.3.1"]
-                 [ring/ring-json "0.2.0"] ;; for serializing
+                 [prismatic/schema "0.3.3"]
+                 [compojure "1.3.1"] ;; for routing
+                 [ring "1.3.2"]
+                 [ring/ring-json "0.3.1"] ;; for serializing
                  [http-kit "2.1.19"]
 
-                 [com.taoensso/timbre "3.3.1"] ;; for logging
-
-                 [clj-http "1.0.0"] ;; for http getting and cookieing
+                 [clj-http "1.0.1"] ;; for http getting and cookieing
 
                  [enlive "1.1.5"] ;; for scraping
 
-                 [cheshire "5.3.1"] ;; JSON deserializing
+                 [cheshire "5.4.0"] ;; JSON deserializing
 
-                 [midje "1.6.3"] ;; for testing :P
-
-                 [clojurewerkz/elastisch "2.1.0-beta7"] ;; for persitance
-                 [com.taoensso/carmine "2.7.0" :exclusions [org.clojure/clojure]]] ;; for caching
+                 [clojurewerkz/elastisch "2.1.0"] ;; for persitance
+                 [com.taoensso/carmine "2.9.0"]] ;; for caching
 
   :plugins [[lein-cljsbuild "1.0.4-SNAPSHOT"]]
 
