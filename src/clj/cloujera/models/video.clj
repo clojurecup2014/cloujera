3af6f2822d30c97f412ac05ba43784289dd65e06
Martino Visintin <mvisinti@thoughtworks.com>
2015-02-23 11:13:10 -0500

    remove fake-video

--- src/clj/cloujera/models/video.clj
+++ src/clj/cloujera/models/video.clj
@@ -1,17 +1,6 @@
 (ns cloujera.models.video
   (:require [schema.core :as s]))
 
-(def fake-video
-  {:id "29"
-   :title "Lecture 3 - Chapter 2: The industrial revolution starts in England"
-   :transcript "The world we've seen is divided...(no timestamps!!)"
-   :video-url "http://d396qusza40orc.cloudfront.net/susdev/recoded_videos%2Fsusdev_3_01.da6ff370809111e3af279b48f4519db4.webm"
-   :_link "https://class.coursera.org/susdev-002/lecture/29"
-   :course {:id "susdev-002"
-            :title "The Age of Sustainable Development"
-            :_link "https://class.coursera.org/susdev-002"}})
-
-
 (def Course
   "The schema for a Coursera Course (embedded in a video)"
   {:id s/Str
